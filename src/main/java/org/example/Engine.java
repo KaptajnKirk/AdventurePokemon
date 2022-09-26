3148067056c52f747ff38a24d88f9903004e07e5
Hytting7 <thom4a12@stud.kea.dk>
2022-09-26 15:44:44 +0200

    Working Menu prototype

--- src/main/java/org/example/Engine.java
+++ src/main/java/org/example/Engine.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import java.util.Map;
+import java.util.Scanner;
+
 public class Engine {
+    Scanner scanner = new Scanner(System.in);
+
+    //String operation = scanner.next();
+    String choice = scanner.nextLine();
+
+
+    public void game() {
+        System.out.println("Welcome to this game!");
+        switch (choice) {
+            case "Go South","S","South" -> System.out.println("Going South");
+            case "Go North","N","North" -> System.out.println("Going North");
+
+        }
+
+    }
+
+
 }
