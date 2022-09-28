e9e1062b074a5a2fe1ee2d06cb654292d2293c37
Hytting7 <thom4a12@stud.kea.dk>
2022-09-28 10:52:53 +0200

    Player Class created

--- src/main/java/org/example/Adventure.java
+++ src/main/java/org/example/Adventure.java
@@ -5,45 +5,13 @@ import java.util.Scanner;
 
 public class Adventure {
 
-    private Map map;
+
     private UI ui;
+    private Player player;
   public Adventure() {
-      map = new Map();
       ui = new UI();
-      playerposition = map.room1;
+      player = new Player();
   }
-
-    private Rooms playerposition = null;
-
-    public void goEast(){
-      if (playerposition.getEast() == null)
-        System.out.println("You cannot go this way");
-      else
-        playerposition = playerposition.getEast();
-    }
-
-    public void goNorth(){
-      if (playerposition.getNorth() == null)
-        System.out.println("You cannot go this way");
-      else
-        playerposition = playerposition.getNorth();
-    }
-
-  public void goSouth(){
-    if (playerposition.getSouth() == null)
-      System.out.println("You cannot go this way");
-    else
-      playerposition = playerposition.getSouth();
-  }
-
-  public void goWest(){
-    if (playerposition.getWest() == null)
-      System.out.println("You cannot go this way");
-    else
-      playerposition = playerposition.getWest();
-  }
-
-
 
     public void game() {
 
@@ -51,20 +19,18 @@ public class Adventure {
         boolean run = true;
 
         do {
-
-
 //            playerposition.setCurrentPosition(room1);
 
             Scanner scanner = new Scanner(System.in);
             String choice = scanner.nextLine();
 
             switch (choice) {
-                case "go south", "s", "south" -> goSouth();
-                case "go north", "n", "north" -> goNorth();
-                case "go east", "e", "east" -> goEast();
-                case "Go west", "w", "west" -> goWest();
+                case "go south", "s", "south" -> player.goSouth();
+                case "go north", "n", "north" -> player.goNorth();
+                case "go east", "e", "east" -> player.goEast();
+                case "Go west", "w", "west" -> player.goWest();
                 case "help" -> ui.helpMenu();
-                case "look around", "look" -> System.out.println(playerposition.getDescription());
+                case "look around", "look" -> System.out.println(player.getPlayerposition().getDescription());
                 case "end", "end program", "exit", "exit program" -> {
                     System.out.println("Program ended");
                     run = false;
